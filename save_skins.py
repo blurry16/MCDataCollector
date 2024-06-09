a5a7c3a21e221b534f6735aecd61ccb8fd556edd
blurry16 <roma.reznik@gmail.com>
2024-06-09 22:26:25 +0300

    uniting save_skins with save_skin_url

--- save_skins.py
+++ save_skins.py
@@ -1,12 +1,17 @@
 import requests
 from __data__ import cvdbdata
 
+mode = input("1: url, 2: name. ")
+
 data = cvdbdata.load()
 for i in data:
     url = data[i]["skin_url"]
     if url != None:
-        name = data[i]["name"]
-        response = requests.get(url=url)
-        with open(rf"skins\{name}.png", "wb") as file:
-            file.write(response.content)
-        print(f"Saved {name}.png")
+        try:           
+            name = data[i]["name"] if mode == "2" else url[38:]
+            response = requests.get(url=url)
+            with open(rf"skins\{name}.png" if mode == "2" else rf"skins_url\{name}.png", "wb") as file:
+                file.write(response.content)
+            print(f"Saved {name}.png")
+        except:
+            pass
