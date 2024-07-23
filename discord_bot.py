a5a09fd44b9a7b2c3864f4c17c8fbfd55ba2d9d7
blurry16 <roma.reznik@gmail.com>
2024-07-24 02:08:16 +0300

    same thing ive done at main branch :P

--- discord_bot.py
+++ discord_bot.py
@@ -20,6 +20,17 @@ bot = commands.Bot(
 TOKEN = ""
 
 
+async def dbidcheck(db_id: int, inter: disnake.ApplicationCommandInteraction) -> str | None:
+    if db_id < 0:
+        await inter.send("Database ID must be greater or equal 0.", ephemeral=True)
+        return None
+    data = cvdbdata.load()
+    if db_id > len(data):
+        await inter.send("There's no such player with this Database ID.", ephemeral=True)
+        return None
+    return list(data)[db_id]
+
+
 @bot.event
 async def on_ready() -> None:
     print(f"Bot {bot.user} is ready!")
@@ -28,100 +39,200 @@ async def on_ready() -> None:
 @bot.slash_command(
     description="Check last time when a selected player was seen online. (if the bot saw them online)"
 )
-async def lastseen(inter: disnake.ApplicationCommandInteraction, nickname: str) -> None:
-    print(f"{inter.author} used /lastseen {nickname}")
-    data = cvdbdata.load()
-    try:
-        uuid = mapi.get_uuid(nickname)
+async def lastseen(inter: disnake.ApplicationCommandInteraction, nickname: str = None, uuid: str = None,
+                   db_id: int = None) -> None:
+    if nickname is not None and uuid is None and db_id is None:
+        print(f"{inter.author} used /lastseen nickname={nickname}")
+        data = cvdbdata.load()
+        try:
+            uuid = mapi.get_uuid(nickname)
+            if uuid in data:
+                last_seen = data[uuid]["last_seen"]
+                await inter.send(
+                    f"{data[uuid]['name']} was last seen at <t:{last_seen}:f>. "
+                    f"({datetime.fromtimestamp(round(time())) - datetime.fromtimestamp(last_seen)} ago.)"
+                )
+            else:
+                await inter.send("The bot has never seen this player.", ephemeral=True)
+        except errors.NotFound:
+            nickname = nickname.lower()
+            if nickname in data:
+                last_seen = data[nickname]["last_seen"]
+                await inter.send(
+                    f"{data[nickname]['name']} was last seen at <t:{last_seen}:f>. "
+                    f"({datetime.fromtimestamp(round(time())) - datetime.fromtimestamp(last_seen)} ago.)"
+                )
+            else:
+                await inter.send(f"Player {nickname} doesn't exist.", ephemeral=True)
+    elif uuid is not None and nickname is None and db_id is None:
+        print(f"{inter.author} used /lastseen uuid={uuid}")
+        uuid = uuid.replace("-", "")
+        data = cvdbdata.load()
         if uuid in data:
             last_seen = data[uuid]["last_seen"]
             await inter.send(
-                f"{data[uuid]['name']} was last seen at <t:{last_seen}:f>. "
+                f"{data[uuid]['name']} ({uuid}) was last seen at <t:{last_seen}:f>. "
                 f"({datetime.fromtimestamp(round(time())) - datetime.fromtimestamp(last_seen)} ago.)"
             )
         else:
-            await inter.send("The bot has never seen this player.", ephemeral=True)
-    except errors.NotFound:
-        nickname = nickname.lower()
-        if nickname in data:
-            last_seen = data[nickname]["last_seen"]
-            await inter.send(
-                f"{data[nickname]['name']} was last seen at <t:{last_seen}:f>. "
-                f"({datetime.fromtimestamp(round(time())) - datetime.fromtimestamp(last_seen)} ago.)"
-            )
-        else:
-            await inter.send(f"Player {nickname} doesn't exist.", ephemeral=True)
+            await inter.send("There's no such player in the Database with this UUID.", ephemeral=True)
+
+    elif db_id is not None and nickname is None and uuid is None:
+        print(f"{inter.author} used /lastseen db_id={db_id}")
+        uuid = await dbidcheck(db_id, inter)
+        if uuid is None:
+            return
+        data = cvdbdata.load()
+        last_seen = data[uuid]["last_seen"]
+        await inter.send(
+            f"{data[uuid]['name']} ({db_id}) was last seen at <t:{last_seen}:f>. "
+            f"({datetime.fromtimestamp(round(time())) - datetime.fromtimestamp(last_seen)} ago.)"
+        )
+    elif nickname is None and uuid is None and db_id is None:
+        await inter.send("Too few arguments!", ephemeral=True)
+    else:
+        await inter.send("Too many arguments!", ephemeral=True)
 
 
 @bot.slash_command(
     description="Check the first time when the bot has seen selected player."
 )
-async def firsttimeseen(inter: disnake.ApplicationCommandInteraction, nickname: str) -> None:
-    print(f"{inter.author} used /firsttimeseen {nickname}")
-    data = cvdbdata.load()
-    try:
-        uuid = mapi.get_uuid(nickname)
+async def firsttimeseen(inter: disnake.ApplicationCommandInteraction, nickname: str = None, uuid: str = None,
+                        db_id: int = None) -> None:
+    if nickname is not None and uuid is None and db_id is None:
+        print(f"{inter.author} used /firsttimeseen nickname={nickname}")
+        data = cvdbdata.load()
+        try:
+            uuid = mapi.get_uuid(nickname)
+            if uuid in data:
+                first_time_seen = data[uuid]["first_time_seen"]
+                await inter.send(
+                    f"{data[uuid]['name']} was seen for the first time at <t:{first_time_seen}:f>. "
+                    f"({datetime.fromtimestamp(round(time())) - datetime.fromtimestamp(first_time_seen)} ago.)"
+                )
+            else:
+                await inter.send("The bot has never seen this player.", ephemeral=True)
+        except errors.NotFound:
+            nickname = nickname.lower()
+            if nickname in data:
+                first_time_seen = data[nickname]["first_time_seen"]
+                await inter.send(
+                    f"{data[nickname]['name']} was seen for the first time at <t:{first_time_seen}:f>. "
+                    f"({datetime.fromtimestamp(round(time())) - datetime.fromtimestamp(first_time_seen)} ago.)"
+                )
+            else:
+                await inter.send(f"Player {nickname} doesn't exist.", ephemeral=True)
+    elif uuid is not None and nickname is None and db_id is None:
+        print(f"{inter.author} used /firsttimeseen uuid={uuid}")
+        uuid = uuid.replace("-", "")
+        data = cvdbdata.load()
         if uuid in data:
             first_time_seen = data[uuid]["first_time_seen"]
             await inter.send(
-                f"{data[uuid]['name']} was seen for the first time at <t:{first_time_seen}:f>. "
+                f"{data[uuid]['name']} ({uuid}) was seen for the first time at <t:{first_time_seen}:f>. "
                 f"({datetime.fromtimestamp(round(time())) - datetime.fromtimestamp(first_time_seen)} ago.)"
             )
         else:
-            await inter.send("The bot has never seen this player.", ephemeral=True)
-    except errors.NotFound:
-        nickname = nickname.lower()
-        if nickname in data:
-            first_time_seen = data[nickname]["first_time_seen"]
-            await inter.send(
-                f"{data[nickname]['name']} was seen for the first time at <t:{first_time_seen}:f>. "
-                f"({datetime.fromtimestamp(round(time())) - datetime.fromtimestamp(first_time_seen)} ago.)"
-            )
-        else:
-            await inter.send(f"Player {nickname} doesn't exist.", ephemeral=True)
+            await inter.send("There's no such player in the Database with this UUID.", ephemeral=True)
+
+    elif db_id is not None and nickname is None and uuid is None:
+        print(f"{inter.author} used /firsttimeseen db_id={db_id}")
+        uuid = await dbidcheck(db_id, inter)
+        if uuid is None:
+            return
+        data = cvdbdata.load()
+        first_time_seen = data[uuid]["first_time_seen"]
+        await inter.send(
+            f"{data[uuid]['name']} ({db_id}) was seen for the first time at <t:{first_time_seen}:f>. "
+            f"({datetime.fromtimestamp(round(time())) - datetime.fromtimestamp(first_time_seen)} ago.)"
+        )
+    elif nickname is None and uuid is None and db_id is None:
+        await inter.send("Too few arguments!", ephemeral=True)
+    else:
+        await inter.send("Too many arguments!", ephemeral=True)
 
 
 @bot.slash_command(description="Get database player's id with their nickname.")
-async def getdbid(inter: disnake.ApplicationCommandInteraction, nickname: str) -> None:
-    print(f"{inter.author} used /getdbid {nickname}")
-    data = cvdbdata.load()
-    try:
-        uuid = mapi.get_uuid(nickname)
+async def getdbid(inter: disnake.ApplicationCommandInteraction, nickname: str = None, uuid: str = None) -> None:
+    if nickname is not None and uuid is None:
+        print(f"{inter.author} used /getdbid nickname={nickname}")
+        data = cvdbdata.load()
+        try:
+            uuid = mapi.get_uuid(nickname)
+            if uuid in data:
+                await inter.send(
+                    f"{data[uuid]['name']}'s database ID is {data[uuid]['db_id']}."
+                )
+            else:
+                await inter.send("The bot has never seen this player.", ephemeral=True)
+        except errors.NotFound:
+            nickname = nickname.lower()
+            if nickname in data:
+                await inter.send(
+                    f"{data[nickname]['name']}'s database ID is {data[nickname]['db_id']}"
+                )
+            else:
+                await inter.send(f"Player {nickname} doesn't exist.", ephemeral=True)
+    elif uuid is not None and nickname is None:
+        print(f"{inter.author} used /getdbid uuid={uuid}")
+        uuid = uuid.replace("-", "")
+        data = cvdbdata.load()
         if uuid in data:
             await inter.send(
-                f"{data[uuid]['name']}'s database ID is {data[uuid]['db_id']}."
+                f"{data[uuid]['name']}'s ({uuid}) database ID is {data[uuid]['db_id']}."
             )
         else:
-            await inter.send("The bot has never seen this player.", ephemeral=True)
-    except errors.NotFound:
-        nickname = nickname.lower()
-        if nickname in data:
-            await inter.send(
-                f"{data[nickname]['name']}'s database ID is {data[nickname]['db_id']}"
-            )
-        else:
-            await inter.send(f"Player {nickname} doesn't exist.", ephemeral=True)
+            await inter.send("There's no such player in the Database with this UUID", ephemeral=True)
+    elif nickname is not None and uuid is not None:
+        await inter.send("Too few arguments!", ephemeral=True)
+    else:
+        await inter.send("Too many arguments!", ephemeral=True)
 
 
 @bot.slash_command(
     description="Returns data of selected player from the database in JSON format."
 )
-async def getdata(inter: disnake.ApplicationCommandInteraction, nickname: str, indent: int = 2) -> None:
-    print(f"{inter.author} used /getdata {nickname}")
-    indent = 2 if (0 > indent) or (indent > 20) else indent
-    data = cvdbdata.load()
-    try:
-        uuid = mapi.get_uuid(nickname)
+async def getdata(inter: disnake.ApplicationCommandInteraction, nickname: str = None, uuid: str = None,
+                  db_id: int = None,
+                  indent: int = 2) -> None:
+    if nickname is not None and uuid is None and db_id is None:
+        print(f"{inter.author} used /getdata nickname={nickname}")
+        indent = 2 if (0 > indent) or (indent > 20) else indent
+        data = cvdbdata.load()
+        try:
+            uuid = mapi.get_uuid(nickname)
+            if uuid in data:
+                await inter.send(f"```json\n{json.dumps(data[uuid], indent=indent)}```")
+            else:
+                await inter.send("The bot has never seen this player.", ephemeral=True)
+        except errors.NotFound:
+            nickname = nickname.lower()
+            if nickname in data:
+                await inter.send(f"```json\n{json.dumps(data[nickname], indent=indent)}```")
+            else:
+                await inter.send(f"Player {nickname} doesn't exist.", ephemeral=True)
+    elif uuid is not None and nickname is None and db_id is None:
+        print(f"{inter.author} used /getdata uuid={uuid}")
+        uuid = uuid.replace("-", "")
+        data = cvdbdata.load()
         if uuid in data:
             await inter.send(f"```json\n{json.dumps(data[uuid], indent=indent)}```")
         else:
-            await inter.send("The bot has never seen this player.", ephemeral=True)
-    except errors.NotFound:
-        nickname = nickname.lower()
-        if nickname in data:
-            await inter.send(f"```json\n{json.dumps(data[nickname], indent=indent)}```")
+            await inter.send("There's no such player in the Database with this UUID.", ephemeral=True)
+    elif db_id is not None and nickname is None and uuid is None:
+        print(f"{inter.author} used /getdata db_id={db_id}")
+        uuid = await dbidcheck(db_id, inter)
+        if uuid is None:
+            return
+        data = cvdbdata.load()
+        if uuid in data:
+            await inter.send(f"```json\n{json.dumps(data[uuid], indent=indent)}```")
         else:
-            await inter.send(f"Player {nickname} doesn't exist.", ephemeral=True)
+            await inter.send("There's no such player in the Database with this UUID.", ephemeral=True)
+    elif nickname is None and uuid is None and db_id is None:
+        await inter.send("Too few arguments!", ephemeral=True)
+    else:
+        await inter.send("Too many arguments!", ephemeral=True)
 
 
 @bot.slash_command(description="Check the count of players in the database.")
--- mcdatacollector.py
+++ mcdatacollector.py
@@ -2,7 +2,6 @@ import json
 from os import system
 from os.path import exists, isfile, isdir, splitext
 from pathlib import Path
-from typing import Union
 
 from colorama import init, Back, Fore
 from mojang import API
@@ -52,12 +51,12 @@ class JsonFile:
     def __init__(self, file_path: Path) -> None:
         self.file_path: Path = file_path
 
-    def load(self) -> Union[dict, list]:
+    def load(self) -> dict | list:
         """loads data from json file"""
         with open(self.file_path, "r", encoding="UTF-8") as data_file:
             return json.load(data_file)
 
-    def dump(self, data: Union[dict, list], indent: int = 4) -> None:
+    def dump(self, data: dict | list, indent: int = 4) -> None:
         """dumps selected data to the file"""
         with open(self.file_path, "w", encoding="UTF-8") as data_file:
             json.dump(data, data_file, indent=indent)
