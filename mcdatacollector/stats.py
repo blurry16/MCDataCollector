889aa4ed08a71bcaf71e6fe5ed1df12879293afd
blurry16 <roma.reznik@gmail.com>
2025-01-01 23:22:14 +0300

    Fix stats

--- mcdatacollector/stats.py
+++ mcdatacollector/stats.py
@@ -10,7 +10,8 @@ def savestats():
     data_len = len(datafile.load())
     statsdata: dict = statsdataobj.load()
     last_date = list(statsdata)[-1]
-    now_date = (datetime.now().date() - timedelta(days=1)).strftime("%Y-%m-%d")
+    # now_date = (datetime.now().date() - timedelta(days=1)).strftime("%Y-%m-%d")
+    now_date = (datetime.now().date()).strftime("%Y-%m-%d")
     print(now_date)
     statsdata[now_date] = {
         "count": data_len,
